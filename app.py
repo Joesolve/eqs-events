998ec5dfe7167ba38b768ce10b0da03ad0bfb632
Joesolve <josephelliecooper@gmail.com>
2025-11-07 22:36:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def login_page():
             background-color: white;
         }
         .stButton>button {
-            background-color: black;
+            background-color: #FF6B35;
             color: white;
             border: none;
             padding: 10px 24px;
